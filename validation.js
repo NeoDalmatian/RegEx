29354f7c286cc3a82af0af4b025137047d7df05c
NeoDalmatian <aviskovic100@gmail.com>
2022-07-04 14:16:54 +0200

    Make RegEx for username, email, password and slug

--- validation.js
+++ validation.js
@@ -1,15 +1,19 @@
 const inputs = document.querySelectorAll("input"); //this is HTML collection
 
 const patterns = {
-  telephone:/^\d{11}$/
+  username:/^[a-z\d]{5,12}$/i,
+  email:/^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
+  password:/^[\w@-]{8,20}$/,
+  telephone:/^\d{11}$/,
+  slug:/^[a-z\d-]{8,20}$/
 };
 
 //validation function
 function validate(field, regex){
   if(regex.test(field.value)){
-    field.className = "valid" //it gives class="valid"
+    field.className = "valid"; //it gives class="valid"
   }else{
-    field.className = "invalid" //it gives class="invalid"
+    field.className = "invalid"; //it gives class="invalid"
   }
 }
 
