0433bc0d666dab1c2aaa3de58f033ade22c7aaeb
NeoDalmatian <aviskovic100@gmail.com>
2022-07-04 13:36:06 +0200

    Grab input with keyUp in JS

--- validation.js
+++ validation.js
@@ -1,3 +1,12 @@
+const inputs = document.querySelectorAll("input"); //this is HTML collection
+
 const patterns = {
   telephone:/^\d{11}$/
 };
+
+//we cant use inputs.addEvent because up above is HTML collection
+inputs.forEach((input) => {
+  input.addEventListener("keyup", (e) => {
+    console.log(e.target.attributes.name.value);
+  });
+});
