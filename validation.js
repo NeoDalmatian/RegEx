3894dca692d5b5bf69485ea0598f2af54817afda
NeoDalmatian <aviskovic100@gmail.com>
2022-07-04 13:49:27 +0200

    Add validation function in JS

--- validation.js
+++ validation.js
@@ -4,9 +4,19 @@ const patterns = {
   telephone:/^\d{11}$/
 };
 
+//validation function
+function validate(field, regex){
+  if(regex.test(field.value)){
+    field.className = "valid" //it gives class="valid"
+  }else{
+    field.className = "invalid" //it gives class="invalid"
+  }
+}
+
 //we cant use inputs.addEvent because up above is HTML collection
 inputs.forEach((input) => {
   input.addEventListener("keyup", (e) => {
-    console.log(e.target.attributes.name.value);
+    //console.log(e.target.attributes.name.value);
+    validate(e.target, patterns[e.target.attributes.name.value])
   });
 });
